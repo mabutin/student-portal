d3b86ac6931cdc9b4f385df8e4dc44e1c43f4b03
mnlyndm <millaminaminalyn@gmail.com>
2023-12-08 14:44:59 +0800

    Form with database connected to sql and popup message indicating student number, password, and a button directed to login page, download txt file using blob, email and form verification.

---
+++ php/pre-registration.php
@@ -0,0 +1,74 @@
+    <?php
+
+    include 'conn.php';
+
+    header('Content-Type: application/json'); 
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
+        error_log(json_encode($_POST));
+        $course = $_POST["course"];
+        $surname = $_POST["stdSurname"];
+        $first_name = $_POST["stdFirstname"];
+        $middle_name = $_POST["stdMiddlename"];
+        $suffix = $_POST["stdSuffix"];
+        $city = $_POST["stdCity"];
+        $email = $_POST["email"];
+        $mobile = $_POST["stdMobile"];
+
+        $firstLetterFirstName = ord(strtoupper(substr($first_name, 0, 1))) - ord('A') + 1;
+        $firstLetterSurname = ord(strtoupper(substr($surname, 0, 1))) - ord('A') + 1;
+
+        $studentNumber = strtoupper($firstLetterFirstName . date("His") . $firstLetterSurname);
+
+        $password = substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 8);
+
+        $sqlStudent = "INSERT INTO students (surname, first_name, middle_name, suffix) VALUES ('$surname','$first_name','$middle_name','$suffix')";
+        if ($conn->query($sqlStudent) === TRUE) {
+            $studentId = $conn->insert_id;
+
+            $sqlCourse = "INSERT INTO course (name) VALUES ('$course')";
+            if ($conn->query($sqlCourse) === TRUE) {
+                $courseId = $conn->insert_id;
+
+                $sqlContactInformation = "INSERT INTO contact_information (city, email, mobile_number) VALUES ('$city','$email','$mobile')";
+                if ($conn->query($sqlContactInformation) === TRUE) {
+                    $contactInformationId = $conn->insert_id;
+
+                    $sqlStudentNumber = "INSERT INTO student_number (student_number) VALUES ('$studentNumber')";
+                    if ($conn->query($sqlStudentNumber) === TRUE) {
+                        $studentNumberId = $conn->insert_id;
+
+                        $sqlSchoolAccount = "INSERT INTO school_account (student_number_id, password) VALUES ('$studentNumberId', '$password')";
+                        if ($conn->query($sqlSchoolAccount) === TRUE) {
+                            $schoolAccountId = $conn->insert_id;
+
+                            $sqlStudentInformation = "INSERT INTO student_information (students_id, contact_information_id, course_id, school_account_id, status) VALUES ('$studentId','$contactInformationId', '$courseId', '$schoolAccountId','pre-registered')";
+                            if ($conn->query($sqlStudentInformation) === TRUE) {
+                                $result = [
+                                    "studentNumber" => $studentNumber,
+                                    "password" => $password,
+                                    "status" => "recorded"
+                                ];
+                                echo json_encode($result);
+                            } else {
+                                echo json_encode(["status" => "error", "message" => "Error inserting into student information"]);
+                            }
+                        } else {
+                            echo json_encode(["status" => "error", "message" => "Error inserting into school account: " . $conn->error]);
+                        }
+                    } else {
+                        echo json_encode(["status" => "error", "message" => "Error inserting into student number: " . $conn->error]);
+                    }
+                } else {
+                    echo json_encode(["status" => "error", "message" => "Error inserting into contact information: " . $conn->error]);
+                }
+            } else {
+                echo json_encode(["status" => "error", "message" => "Error inserting into course: " . $conn->error]);
+            }
+        } else {
+            echo json_encode(["status" => "error", "message" => "Error inserting into students: " . $conn->error]);
+        }
+    } else {
+        echo json_encode(["status" => "error", "message" => "Invalid request"]);
+    }
+    ?>
