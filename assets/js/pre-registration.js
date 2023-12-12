cc3db42b7c412cb4fa9227c64a3bfed4c398fae9
mnlyndm <millaminaminalyn@gmail.com>
2023-12-12 13:42:06 +0800

    Email verificationa and name of login updated

--- assets/js/pre-registration.js
+++ assets/js/pre-registration.js
@@ -1,5 +1,23 @@
+function validateEmail(email) {
+    const regex = /\S+@\S+\.\S+/;
+    return regex.test(email);
+}
+
+function displayError(message) {
+    // Show error message to the user, e.g., in a popup or an error message container
+    const errorContainer = document.getElementById('errorContainer');
+    errorContainer.textContent = message;
+    errorContainer.classList.remove('hidden');
+}
+
 function submitForm() {
-    validateEmail();
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+
+    if (!validateEmail(email)) {
+        displayError('Invalid email format. Example: example@gmail.com');
+        return false; 
+    }
 
     const formData = new FormData(document.querySelector('form'));
     formData.append('submit', 'Submit');
@@ -8,40 +26,42 @@ function submitForm() {
         method: 'POST',
         body: new URLSearchParams(formData),
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Server response:', data);
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Server response:', data);
 
-            if (data && data.status === 'recorded') {
-                document.getElementById('studentNumber').textContent = data.studentNumber;
-                document.getElementById('password').textContent = data.password;
+        if (data && data.status === 'recorded') {
+            document.getElementById('studentNumber').textContent = data.studentNumber;
+            document.getElementById('password').textContent = data.password;
 
-                document.getElementById('popup').classList.remove('hidden');
+            document.getElementById('popup').classList.remove('hidden');
 
-                const blobData = new Blob(
-                    [`Student Number: ${data.studentNumber}\nPassword: ${data.password}`],
-                    { type: 'text/plain' }
-                );
+            const blobData = new Blob(
+                [`Student Number: ${data.studentNumber}\nPassword: ${data.password}`],
+                { type: 'text/plain' }
+            );
 
-                const downloadLink = document.createElement('a');
-                downloadLink.href = window.URL.createObjectURL(blobData);
-                downloadLink.download = 'student_credentials.txt';
+            const downloadLink = document.createElement('a');
+            downloadLink.href = window.URL.createObjectURL(blobData);
+            downloadLink.download = 'student_credentials.txt';
 
-                downloadLink.click();
+            downloadLink.click();
 
-                window.URL.revokeObjectURL(downloadLink.href);
-            } else {
-                console.error('Unexpected server response:', data);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching or processing data:', error);
-        });
+            window.URL.revokeObjectURL(downloadLink.href);
+        } else {
+            console.error('Unexpected server response:', data);
+            displayError('Unexpected server response');
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching or processing data:', error);
+        displayError('Error fetching or processing data');
+    });
 
     return false;
 }
