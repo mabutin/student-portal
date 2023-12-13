f242b6f59cb38337f7404fc42920a4d0ac625a65
Kuriso05 <debieaguirre@gmail.com>
2023-12-14 03:22:44 +0800

    deleted:    student/dashboard.html modified:   student/dashboard.php title to Student Dashboard

--- student/dashboard.php
+++ student/dashboard.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../assets/css/custom.css">
-    <title>sidebar</title>
+    <title>Student Dashboard</title>
 </head>
 
 <body class="font-serif"> 
