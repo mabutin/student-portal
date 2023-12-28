57a66bd26e426059c29359e32720d5f42319cdaa
Br4ndo-M <manoscabrnd06@gmail.com>
2023-12-28 23:20:03 +0800

    RMS: Added sidebar menu

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,5 +1,6 @@
 <?php
-function hasAccess($allowedUserTypes, $currentUserType) {
+function hasAccess($allowedUserTypes, $currentUserType)
+{
     return in_array($currentUserType, $allowedUserTypes);
 }
 ?>
@@ -15,7 +16,7 @@ function hasAccess($allowedUserTypes, $currentUserType) {
     <title>sidebar</title>
 </head>
 
-<body class="font-serif"> 
+<body class="font-serif">
     <form action="../login/admin/logout.php">
         <div id="sidebar" class="flex flex-col justify-between sidebar w-56 h-screen ease-linear duration-500 cursor-pointer sidebar-default" style="background-color: #4D81C8;">
             <div>
@@ -23,11 +24,11 @@ function hasAccess($allowedUserTypes, $currentUserType) {
                     <div class="logo-container">
                         <img src="../assets/svg/ollcLogoNoName.svg" class="logo w-10 h-10" alt="">
                     </div>
-                    <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="textDiv" style="letter-spacing: 2px;"> 
+                    <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="textDiv" style="letter-spacing: 2px;">
                         <span>
                             OUR LADY OF LOURDES
                         </span>
-                        <span> 
+                        <span>
                             COLLEGE
                         </span>
                     </div>
@@ -38,59 +39,72 @@ function hasAccess($allowedUserTypes, $currentUserType) {
                             <div class="logo-container">
                                 <img src="../assets/svg/dashboard.svg" class="logo w-6 h-6" alt="">
                             </div>
-                            <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="dashboardText" style="letter-spacing: 2px;"> 
+                            <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="dashboardText" style="letter-spacing: 2px;">
                                 DASHBOARD
                             </div>
                         </a>
                     </div>
 
-                    <?php if (hasAccess(['Admin', 'Developer'], $usertype)): ?>
+                    <?php if (hasAccess(['Admin', 'Developer'], $usertype)) : ?>
                         <div class="ml-2">
                             <a href="account-management.php" class="px-2 flex items-center justify-start h-10 gap-3">
                                 <div class="logo-container">
                                     <img src="../assets/svg/account-management.svg" class="logo w-6 h-6" alt="">
                                 </div>
-                                <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="accountManagementText" style="letter-spacing: 2px;"> 
+                                <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="accountManagementText" style="letter-spacing: 2px;">
                                     Account Management
                                 </div>
                             </a>
                         </div>
                     <?php endif; ?>
 
-                    <?php if (hasAccess(['Admission','Admin', 'Developer'], $usertype)): ?>
+                    <?php if (hasAccess(['Admission', 'Admin', 'Developer'], $usertype)) : ?>
                         <div class="ml-2">
                             <a href="student-information.php" class="px-2 flex items-center justify-start h-10 gap-3">
                                 <div class="logo-container">
                                     <img src="../assets/svg/student.svg" class="logo w-6 h-6" alt="">
                                 </div>
-                                <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="studentInformationText" style="letter-spacing: 2px;"> 
+                                <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="studentInformationText" style="letter-spacing: 2px;">
                                     Student Information
                                 </div>
                             </a>
                         </div>
                     <?php endif; ?>
 
-                    <?php if (hasAccess(['Faculty','Admin', 'Developer'], $usertype)): ?>
+                    <?php if (hasAccess(['Faculty', 'Admin', 'Developer'], $usertype)) : ?>
                         <div class="ml-2">
                             <a href="faculty.php" class="px-2 flex items-center justify-start h-10 gap-3">
                                 <div class="logo-container">
                                     <img src="../assets/svg/professor.svg" class="logo w-6 h-6" alt="">
                                 </div>
-                                <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="facultyText" style="letter-spacing: 2px;"> 
+                                <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="facultyText" style="letter-spacing: 2px;">
                                     Faculty
                                 </div>
                             </a>
                         </div>
                     <?php endif; ?>
 
-                    <?php if (hasAccess(['Admin', 'Developer', 'College Registrar'], $usertype)): ?>
+                    <?php if (hasAccess(['Admin', 'Developer', 'College Registrar'], $usertype)) : ?>
                         <div class="ml-2">
                             <a href="enrollment-list.php" class="px-2 flex items-center justify-start h-10 gap-3">
                                 <div class="logo-container">
                                     <img src="../assets/svg/enrollment-list.svg" class="logo w-6 h-6" alt="">
                                 </div>
-                                <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="enrollmentListText" style="letter-spacing: 2px;"> 
+                                <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="enrollmentListText" style="letter-spacing: 2px;">
                                     Enrollment List
+                                </div>
+                            </a>
+                        </div>
+                    <?php endif; ?>
+
+                    <?php if (hasAccess(['Admin', 'Developer', 'College Registrar'], $usertype)) : ?>
+                        <div class="ml-2">
+                            <a href="student-record.php" class="px-2 flex items-center justify-start h-10 gap-3">
+                                <div class="logo-container">
+                                    <img src="../assets/svg/student-record-icon.png" class="logo w-6 h-6" alt="">
+                                </div>
+                                <div class="tracking-wide justify-start items-center text-center text-white text-xs hidden" id="enrollmentListText" style="letter-spacing: 2px;">
+                                    Student Record
                                 </div>
                             </a>
                         </div>
