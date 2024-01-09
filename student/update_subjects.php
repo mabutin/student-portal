e2fe38282530ce934b222431ab48973d025920a1
mnlyndm <millaminaminalyn@gmail.com>
2024-01-09 17:13:49 +0800

    Tabe design added

--- student/update_subjects.php
+++ student/update_subjects.php
@@ -35,20 +35,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $result = $stmt->get_result();
 
+        // Output the updated subject table structure
+        echo "<table class='table-auto w-full'>
+                <thead>
+                    <tr class='justify-between bg-blue-200'>
+                        <td style='width: 20%;' class='text-center'>Code</td>
+                        <td style='width: 50%;' class='text-center'>Subject</td>
+                        <td style='width: 30%;' class='text-center'>Units</td>
+                    </tr>
+                </thead>
+                <tbody>";
+
         if ($result->num_rows > 0) {
-            // Output the updated subject table
+            // Iterate through the table rows
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>
-                    <td class='pl-16 hidden'><input type=\"text\" name=\"enrolled_subjects[]\" value=\"{$row['subject_id']}\"></td>
-                    <td class='pl-16'>{$row['code']}</td>
-                    <td class='text-center'>{$row['name']}</td>
-                    <td class='text-center'>{$row['unit']}</td>
-                </tr>";
+                        <td class='pl-16 hidden'><input type=\"text\" name=\"enrolled_subjects[]\" value=\"{$row['subject_id']}\"></td>
+                        <td class='pl-16'>{$row['code']}</td>
+                        <td class='text-center'>{$row['name']}</td>
+                        <td class='text-center'>{$row['unit']}</td>
+                    </tr>";
             }
         } else {
             // Output a message if no subjects are found
             echo "<tr><td colspan='4' class='text-center'>No subjects found</td></tr>";
         }
+
+        // Close the table structure
+        echo "</tbody></table>";
 
         $stmt->close();
     } else {
